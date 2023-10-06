7ea875394e4f3710159d92e1873eb67d9fadb575
20761A0598 <94432080+20761A0598@users.noreply.github.com>
2023-10-06 15:18:19 +0530

    Add files via upload

---
+++ src/test/java/com/lbrce/devops/UserValidationTesting.java
@@ -0,0 +1,32 @@
+package com.lbrce.devops;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserValidationTesting {
+	@Test
+	public void testCase1()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("srinu", "srinu@12345"));
+	}
+	@Test
+	public void testCase2()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(true,uv.check("lbrce", "lbrce@76"));
+	}
+	@Test
+	public void testCase3()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("prasanna", "prasanna@12345"));
+	}
+	@Test
+	public void testCase4()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("Prasanna", "Prasanna@12345"));
+	}
+
+}
